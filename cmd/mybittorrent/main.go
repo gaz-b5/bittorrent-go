606008c6d44210821c92fda21ef885479a9f3185
Rohit Chalak <rohit.s.chalak@gmail.com>
2024-08-22 23:33:02 +0530

    pass stage

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -31,6 +31,8 @@ func decodeBencode(bencodedString string) (interface{}, error) {
 		}
 
 		return bencodedString[firstColonIndex+1 : firstColonIndex+1+length], nil
+	} else if rune(bencodedString[0]) == 'i' {
+		return strconv.Atoi(bencodedString[1 : len(bencodedString)-1])
 	} else {
 		return "", fmt.Errorf("Only strings are supported at the moment")
 	}
@@ -41,15 +43,15 @@ func main() {
 	command := os.Args[1]
 
 	if command == "decode" {
-		
+
 		bencodedValue := os.Args[2]
-		
+
 		decoded, err := decodeBencode(bencodedValue)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-		
+
 		jsonOutput, _ := json.Marshal(decoded)
 		fmt.Println(string(jsonOutput))
 	} else {
